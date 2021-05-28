8c537e5b0355b6f963e9a030db047fc55334be91
dongzhi.wang <915285010@qq.com>
2021-05-28 22:04:05 +0800

    feat(QRCode):init
    init

---
+++ src/test/java/com/mine/qrcode/QrCodeRecogitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeRecogitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
